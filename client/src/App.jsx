ed82edfb0498e94a1e756554d05f385e7936eafa
adexfit <y.adeboye@gmail.com>
2025-11-11 08:57:31 +0100

    initial frontend setup react vite shadcn tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-amber-900 text-3xl underline">Hello</h1>
+    </>
+  );
+}
+
+export default App;
