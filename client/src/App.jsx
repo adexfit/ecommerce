979bf64ccd3172d5617ae9841b5da4f0a5ad3c7e
adexfit <y.adeboye@gmail.com>
2025-11-11 12:15:38 +0100

    added shadcn components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Button } from "@/components/ui/button";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1 className="text-amber-900 text-3xl underline">Hello</h1>
+      <Button>click me</Button>
     </>
   );
 }
